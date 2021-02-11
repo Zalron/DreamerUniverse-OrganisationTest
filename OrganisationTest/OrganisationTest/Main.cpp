13c98fb6137dbf3aa1ce2f9f09730781c6404cb3
Zalron <davidrlawson@optusnet.com.au>
2021-02-11 16:04:51 +1100

    Added unit cpp and h files with beginnings of code

--- OrganisationTest/OrganisationTest/Main.cpp
+++ OrganisationTest/OrganisationTest/Main.cpp
@@ -4,9 +4,5 @@ int main(int argc, char* argv[])
 {
     flecs::world ecs;
 
-   /* ecs.entity("MyEntity")
-        .set<Position>({ 0, 0 })
-        .set<Velocity>({ 1, 1 });*/
-
     while (ecs.progress()) {}
 }
---
+++ OrganisationTest/OrganisationTest/Unit.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+#include <cstdint>
+#include "flecs.h"
+
+struct Unit 
+{
+	int organisationLevel;
+	int maxSubUnits;
+	int totalSubUnits;
+
+};
+class Unit
+{
+public:
+
+};
+
