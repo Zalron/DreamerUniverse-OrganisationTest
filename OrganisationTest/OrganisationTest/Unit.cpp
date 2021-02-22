ce95fd8c0e794d9f16b40c35051de57c3a843fdf
Zalron <davidrlawson@optusnet.com.au>
2021-02-22 21:47:19 +1100

    add an if statement for child spawning

--- OrganisationTest/OrganisationTest/Unit.cpp
+++ OrganisationTest/OrganisationTest/Unit.cpp
@@ -4,8 +4,11 @@ void Unit::CreateUnit(flecs::iter& iter, SpawnUnit* su)
 {
 	for (auto it : iter)
 	{
-		iter.entity(it).set<UnitBase>({su->organisationLevelSpawning,0,10});
+		iter.entity(it).set<UnitBase>({ su->organisationLevelSpawning,0,10 });
+		if (su->shouldSpawnWithSubUnits == true) 
+		{
+			iter.world().entity("ChildUnit").add_childof(it).set<UnitBase>({ su->organisationLevelSpawning, 0, 10 });
+		}
 		iter.entity(it).remove<SpawnUnit>();
-		iter.world().entity("ChildUnit").set<UnitBase>(su->organisationLevelSpawning, 0, 10);
 	}
 }
